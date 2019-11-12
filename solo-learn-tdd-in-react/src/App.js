a17874bb7afdcba2b3689dd805f3a69035295ada
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-11-12 14:13:50 -0500

    tutorial complete

--- solo-learn-tdd-in-react/src/App.js
+++ solo-learn-tdd-in-react/src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import NewMessageForm from './components/NewMessageForm'
+import MessageList from "./components/MessageList"
 
 const App = () => {
+  const [messages, setMessages] = useState([]);
+  const handleSend = newMessage => {
+    setMessages([newMessage, ...messages])
+  };
   return (
     <div>
+      <NewMessageForm onSend={handleSend}/>
+      <MessageList data={messages} />
     </div>
   );
 }
